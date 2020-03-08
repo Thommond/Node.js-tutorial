6b806218bb8dc422e73a5bbc3831b71f24c5fc1d
Thom Mondeaux <Thommond554@gmail.com>
2020-03-08 15:34:21 -0400

    I added several files to the project including two directories
    The main goal accomplished was to set up a basic server with node.js
    
    The files which are set up on the server when ran is reference and pulic dir.
    
    Inside they have a variety of js files for tips and functionality

--- index.js
+++ index.js
@@ -1,5 +1,87 @@
- const Person = require('./person');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-joe = new Person('Joe', '73')
+const server = http.createServer((req, res) => {
+//   console.log(req.url);
+//
+//   if(req.url === '/') {
+//     fs.readFile(path.join(__dirname, 'public', 'index.html'),
+//     (err, content) => {
+//       if(err) throw err;
+//       res.writeHead(200, { 'Content-Type': 'text/html' });
+//       res.end(content);
+//     }
+//   );
+// }
+//
+//
+//   if(req.url === '/about') {
+//     fs.readFile(path.join(__dirname, 'public', 'about.html'),
+//     (err, content) => {
+//       if(err) throw err;
+//       res.writeHead(200, { 'Content-Type': 'text/html' });
+//       res.end(content);
+//         }
+//       );
+//     }
 
-joe.greeting()
+// Build file path
+let filePath = path.join(
+  __dirname,
+  'public',
+  req.url === '/' ? 'index.html' : req.url
+);
+
+// Extention of the file
+let extname = path.extname(filePath);
+
+ // Intitial content Type
+ let contentType = 'text/html'
+
+ // Check ext and set con tent type
+
+ switch (extname) {
+   case '.js':
+    contentType = 'text/javascript';
+    break;
+   case '.css':
+    contentType = 'text/css';
+    break;
+   case '.json':
+    contentType = 'application/json';
+    break;
+   case '.png':
+    contentType = 'image/png';
+    break;
+   case '.jpg':
+    contentType = 'image/jpg';
+    break;
+ }
+ // Read file
+
+ fs.readFile(filePath, (err, content) => {
+   if (err) {
+     if (err.code == "ENOENT") {
+       // Page not found
+       fs.readFile(path.join(__dirname, 'public', '404.html'),
+     (err, content) => {
+       res.writeHead(200, {'Content-Type': 'text/html' });
+       res.end(content, 'utf8');
+     })
+   } else {
+     // Some server error
+     res.writeHead(500);
+     res.end(`Server Error: ${err.code}`);
+    }
+  } else {
+    //Success
+    res.writeHead(200, { 'Content-Type': contentType });
+    res.end(content, 'utf8');
+  }
+ });
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
